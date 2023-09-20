b4d335de6f496061b312572c1ef02a21918a6dd4
Harica404 <68884486+ericljx2020-gmail@users.noreply.github.com>
2023-09-20 13:38:49 -0700

    fight function completed

--- static/js/player/kyo.js
+++ static/js/player/kyo.js
@@ -10,14 +10,15 @@ export class Kyo extends Player {
 
     init_animations() {
         let outer = this;
+        let offsets = [0,-22,-22,-140,0,0,0];
         for (let i = 0; i < 7; i++) {
             let gif = GIF();
-            gif.load(`/static/images/player/kyo${i}.gif`);
+            gif.load(`/static/images/player/kyo/${i}.gif`);
             this.animations.set(i, {
                 gif:gif,
                 frame_cnt: 0,
-                frame_rate: 5,
-                offset_y: 0,
+                frame_rate: 10,
+                offset_y: offsets[i],
                 loaded: false,
                 scale: 2,
             });
@@ -26,6 +27,10 @@ export class Kyo extends Player {
                 let obj = outer.animations.get(i);
                 obj.frame_cnt = gif.frames.length;
                 obj.loaded = true;
+
+                if (i === 3){
+                    obj.frame_rate = 12;
+                }
             };
         }
     }
